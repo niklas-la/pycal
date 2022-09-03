962067c948992666800c66c9768a89a63093a87a
Niklas Lang <niklas.lang01@gmail.com>
2022-09-04 01:20:05 +0200

    Changed argument to option

--- pycal.py
+++ pycal.py
@@ -30,9 +30,9 @@ class iCalender(object):
 
 
 def main(path: str = typer.Argument(...,help="Path to iCalendar file"),
-         start: Optional[str] = typer.Argument(
+         start: Optional[str] = typer.Option(
              None, help="Start of the time period in the format year-month-day"),
-         stop: Optional[str] = typer.Argument(None, help="End of the time period in the format year-month-day")):
+         stop: Optional[str] = typer.Option(None, help="End of the time period in the format year-month-day")):
     """A tool to extract time spans from iCalendar files."""
     ical = iCalender(path)
     events = ical.get_range(start, stop)

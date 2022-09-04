ab33318e01452e27b23000b2b184ce5d3fcc7fde
Niklas Lang <niklas.lang01@gmail.com>
2022-09-04 16:57:32 +0200

    Add message of removed items

--- pycal.py
+++ pycal.py
@@ -11,6 +11,9 @@ class iCalender(object):
     def __init__(self, filename):
         self.filename = filename
 
+    def __len__(self):
+        return len(self.get_calendar().events)
+
     def get_calendar(self):
         with open(self.filename) as f:
             ical = Calendar(f.read())
@@ -24,20 +27,22 @@ class iCalender(object):
                 event for event in ical.events if event.begin >= arrow.get(start)]
         if stop:
             ical.events = [event for event in ical.events if event.end <=
-                      arrow.get(stop)]
+                           arrow.get(stop)]
 
         return ical
 
 
-def main(path: str = typer.Argument(...,help="Path to iCalendar file"),
+def main(path: str = typer.Argument(..., help="Path to iCalendar file"),
          start: Optional[str] = typer.Option(
              None, help="Start of the time period in the format year-month-day"),
          stop: Optional[str] = typer.Option(None, help="End of the time period in the format year-month-day")):
     """A tool to extract time spans from iCalendar files."""
     ical = iCalender(path)
+    count = len(ical)
     events = ical.get_range(start, stop)
     with open(path, 'w') as f:
         f.writelines(events.serialize_iter())
+    typer.echo(f"Removed {count - len(events.events)} events from {path}")
 
 
 if __name__ == "__main__":
